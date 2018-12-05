530046c3a74735508fc15d13866a93c2c4781e21
root <nicholasabiegel@lewisu.edu>
2018-12-05 07:04:34 +0000

    SCRMTM1-24

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-
+import { AuthService } from '../../auth.service';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -12,32 +12,32 @@ export class UploadComponent implements OnInit {
   videoFile:File;
   thumbFile:File;
 
-  constructor(private http: HttpClient) {
+  constructor(private auth: AuthService, private http: HttpClient) {
   }
 
   ngOnInit() {
   }
 
   onSubmit(f: NgForm): void{
-    let user_id = '1';
+    let user_id = this.auth.profile().sub;
     const formData = new FormData();
     formData.append('userId', user_id);
     formData.append('videoTitle', f.value.videoTitle)
     formData.append('videoDescription', f.value.videoDescription)
-    this.http.post('http://yetube.dockedfiles.com/upload', formData).subscribe(res => {
+    this.http.post('http://yetube.dockedfiles.com:8080/upload', formData).subscribe(res => {
       console.log(res)
       let in_id = res['inserted_id'];
       const formData2 = new FormData();
       formData2.append('userId', user_id);
       formData2.append('videoId', in_id);
       formData2.append('videoFile', this.videoFile, this.videoFile.name);
-      this.http.post('http://yetube.dockedfiles.com/upload/video', formData2).subscribe(res => {
+      this.http.post('http://yetube.dockedfiles.com:8080/upload/video', formData2).subscribe(res => {
 	console.log(res)
         const formData3 = new FormData();
         formData3.append('userId', user_id);
         formData3.append('videoId', in_id);
         formData3.append('thumbFile', this.thumbFile, this.thumbFile.name);
-        this.http.post('http://yetube.dockedfiles.com/upload/thumb', formData3).subscribe(res => {
+        this.http.post('http://yetube.dockedfiles.com:8080/upload/thumb', formData3).subscribe(res => {
           console.log(res)
           console.log("Video upload complete!")
         });
