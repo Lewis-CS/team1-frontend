704ccdd5287883fb07386e1f29c9dead9ec80ee9
root <nicholasabiegel@lewisu.edu>
2018-12-05 07:06:04 +0000

    SCRMTM1-11 SCRMTM1-14 SCRMTM1-15 The view component where you can view, comment and rate

--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { VideoService } from '../../video.service';
-
+import { AuthService } from '../../auth.service';
+import { CommentService } from '../../comment.service';
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
@@ -9,12 +10,13 @@ import { VideoService } from '../../video.service';
 })
 export class ViewComponent implements OnInit {
   
-  constructor(private route:ActivatedRoute, private videos:VideoService) { }
+  constructor(private route:ActivatedRoute, private videos:VideoService, public auth:AuthService, private comments: CommentService) { }
 
   videoId: string;
   recommended: Object;
   
   video: Object;
+  commentss: Object[];
 
   ngOnInit() {
     this.videoId = this.route.snapshot.params['id'];
@@ -25,9 +27,31 @@ export class ViewComponent implements OnInit {
   getVideo(): void{
     this.videos.getVideo(this.videoId).subscribe(res => {
       this.video = res;
+      this.getComments();
     });
   }
+
+  rateVideo(liked): void{
+    if(this.auth.profile() === null) return;
+    this.videos.postRate(this.video['video_id'], this.auth.profile().sub, liked).subscribe(res => {
+      this.video['likes'] = res['likes'];
+      this.video['total'] = res['total'];
+    });
+  }
+
   getRecommended(): void{
     this.videos.getTop5s().subscribe(vid => this.recommended = vid);    
+  }
+  
+  getComments(): void{
+    this.comments.getComments(this.video['video_id']).subscribe(res => {
+      this.commentss = res;
+    });
+  }
+  onComment(form): void{
+    this.comments.postComment(form.value.comment, this.auth.profile().sub, this.video['video_id']).subscribe(res => {
+      console.log(res);
+      this.commentss = res;
+    });
   }
 }
