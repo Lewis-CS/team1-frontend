a8937bdd6c1f652b2a1588da7a37c7b7fb4edad6
root <nicholasabiegel@lewisu.edu>
2018-12-05 07:08:50 +0000

    SCRMTM1-24 User stuff to be able to upload videos

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import * as auth0 from 'auth0-js';
+import { HttpClient } from '@angular/common/http';
+import * as jwt_decode from 'jwt-decode';
 
 (window as any).global = window;
 @Injectable({
@@ -12,10 +14,12 @@ export class AuthService {
     clientID: 'Qnu--PquijnwVGjT8MQUg3jcdyu0ivE3',
     domain: 'yetube.auth0.com',
     responseType: 'token id_token',
-    redirectUri: 'http://142.93.122.253:4200/authorize',
+    //redirectUri: 'http://142.93.122.253:4200/authorize',
+    redirectUri: 'http://yetube.dockedfiles.com:88/authorize',
     scope: 'openid'
   });
-  constructor(public router: Router) { }
+  
+  constructor(public router: Router, private http: HttpClient) { }
   public login(): void{
     this.auth0.authorize();
   }
@@ -24,7 +28,6 @@ export class AuthService {
       if (authResult && authResult.accessToken && authResult.idToken) {
         window.location.hash = '';
         this.setSession(authResult);
-        this.router.navigate(['/home']);
       } else if (err) {
         this.router.navigate(['/home']);
         console.log(err);
@@ -35,18 +38,31 @@ export class AuthService {
   private setSession(authResult): void {
     // Set the time that the Access Token will expire at
     const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
-  }
 
+    this.http.post('http://yetube.dockedfiles.com:8080/users', {authId: this.profile().sub }).subscribe(
+      res => { console.log(res); }, 
+      err => {}, 
+      () => this.router.navigate(['/home'])
+    );
+
+  }
+  public profile(): any{
+    if(localStorage.getItem('id_token') !== null){
+      return jwt_decode(localStorage.getItem('id_token'))
+    }
+    return null;
+  }
   public logout(): void {
     // Remove tokens and expiry time from localStorage
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
     // Go back to the home route
-    this.auth0.logout({returnTo: 'http://142.93.122.253:4200/'});
+    this.auth0.logout({returnTo: 'http://yetube.dockedfiles.com:88/'});
     //this.router.navigate(['/']);
   }
 
