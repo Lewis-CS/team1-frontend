cba4070065eef5d0a20c9f7453e82174b8be2152
root <nicholasabiegel@lewisu.edu>
2018-12-06 15:23:05 +0000

    SCRMTM1-27 Login stuff

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -4,6 +4,7 @@ import { filter } from 'rxjs/operators';
 import * as auth0 from 'auth0-js';
 import { HttpClient } from '@angular/common/http';
 import * as jwt_decode from 'jwt-decode';
+import { environment } from '../environments/environment';
 
 (window as any).global = window;
 @Injectable({
@@ -14,9 +15,8 @@ export class AuthService {
     clientID: 'Qnu--PquijnwVGjT8MQUg3jcdyu0ivE3',
     domain: 'yetube.auth0.com',
     responseType: 'token id_token',
-    //redirectUri: 'http://142.93.122.253:4200/authorize',
-    redirectUri: 'http://yetube.dockedfiles.com:88/authorize',
-    scope: 'openid'
+    redirectUri: environment.auth + '/authorize',
+    scope: 'openid profile'
   });
   
   constructor(public router: Router, private http: HttpClient) { }
@@ -43,7 +43,7 @@ export class AuthService {
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
 
-    this.http.post('http://yetube.dockedfiles.com:8080/users', {authId: this.profile().sub }).subscribe(
+    this.http.post(environment.api + '/users', {authId: this.profile().sub, picture: this.profile().picture, username:this.profile().nickname }).subscribe(
       res => { console.log(res); }, 
       err => {}, 
       () => this.router.navigate(['/home'])
@@ -62,7 +62,7 @@ export class AuthService {
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
     // Go back to the home route
-    this.auth0.logout({returnTo: 'http://yetube.dockedfiles.com:88/'});
+    this.auth0.logout({returnTo: environment.auth + '/'});
     //this.router.navigate(['/']);
   }
 
